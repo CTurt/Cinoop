a671d9df64321869e575a8264be05843e23ba621
CTurt <cturt@users.noreply.github.com>
2015-02-24 21:45:52 +0000

    Instruction 0x43

--- source/cpu.c
+++ source/cpu.c
@@ -8,7 +8,19 @@ void reset(void) {
 	registers.pc = 0x100;
 }
 
+// http://imrannazar.com/Gameboy-Z80-Opcode-Map
 void cpuStep(void) {
-	printf("Undefined opcode!\n");
-	exit(1);
+	unsigned char instruction = read(registers.pc++);
+	
+	switch(instruction) {
+		// LD B,E
+		case 0x43:
+			registers.b = registers.e;
+			break;
+		
+		default:
+			printf("Undefined instruction %#01x!\n", instruction);
+			exit(1);
+			break;
+	}
 }
