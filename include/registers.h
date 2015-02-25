f54d31def89fcdbc01f8adfc2c827954223f70dc
CTurt <cturt@users.noreply.github.com>
2015-02-25 20:03:57 +0000

    Update registers to use anonymous structs

--- include/registers.h
+++ include/registers.h
@@ -1,15 +1,48 @@
 #pragma once
 
 struct registers {
-	unsigned char a;
-	unsigned char b;
-	unsigned char c;
-	unsigned char d;
-	unsigned char e;
-	unsigned char h;
-	unsigned char l;
-	unsigned short pc;
-	unsigned char flags;
+	struct {
+		union {
+			struct {
+				unsigned char a;
+				unsigned char f;
+			};
+			unsigned short af;
+		};
+	};
+	
+	struct {
+		union {
+			struct {
+				unsigned char b;
+				unsigned char c;
+			};
+			unsigned short bc;
+		};
+	};
+	
+	struct {
+		union {
+			struct {
+				unsigned char d;
+				unsigned char e;
+			};
+			unsigned short de;
+		};
+	};
+	
+	struct {
+		union {
+			struct {
+				unsigned char h;
+				unsigned char l;
+			};
+			unsigned short hl;
+		};
+	};
+	
 	unsigned short sp;
+	unsigned short pc;
+	
 	unsigned char ime;
 } extern registers;
