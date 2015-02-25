1d7daa4f91df76beb389ee67b7e3c2eb1e5fb522
CTurt <cturt@users.noreply.github.com>
2015-02-25 17:40:06 +0000

    Better memory handling

--- include/memory.h
+++ include/memory.h
@@ -8,3 +8,4 @@ extern unsigned char oam[0x100];
 extern unsigned char ram[0x2000];
 
 unsigned char read(unsigned short address);
+void write(unsigned short address, unsigned char value);
--- source/memory.c
+++ source/memory.c
@@ -9,5 +9,36 @@ unsigned char ram[0x2000];
 unsigned char read(unsigned short address) {
 	if(!cart) return 0;
 	
-	return cart[address];
+	if(address <= 0x7fff || (address >= 0xa000 && address <= 0xbfff))
+		return cart[address];
+	
+	else if(address >= 0x8000 && address <= 0x9fff)
+		return vram[address - 0x8000];
+	
+	else if(address >= 0xc000 && address <= 0xdfff)
+		return ram[address - 0xC000];
+	
+	else if(address >= 0xe000 && address <= 0xfdff)
+		return ram[address - 0xe000];
+	
+	else if(address >= 0xfe00 && address <= 0xfeff)
+		return oam[address - 0xfe00];
+	
+	/*else if(address >= 0xff80 && address <= 0xfffe)
+		return highRam[0xff & address];*/
+	
+	return 0;
+}
+
+void write(unsigned short address, unsigned char value) {
+	if(!cart) return;
+	
+	if(address >= 0xc000 && address <= 0xdfff)
+		ram[address - 0xc000] = value;
+	
+	else if(address >= 0xfe00 && address <= 0xfeff)
+		oam[address - 0xfe00] = value;
+	
+	//else if(address >= 0xff80 && address <= 0xfffe)
+		//highRam[0xff & address] = value;
 }
