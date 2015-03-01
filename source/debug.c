4b8428e9ecf8d06c092c3815c36c0ad9d2b0ab38
CTurt <cturt@users.noreply.github.com>
2015-03-01 08:04:40 +0000

    Debug interrupts

--- source/debug.c
+++ source/debug.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "registers.h"
+#include "interrupts.h"
 
 #include "debug.h"
 
@@ -15,5 +16,7 @@ void printRegisters(void) {
 	printf("L: 0x%02x\n", registers.l);
 	printf("SP: 0x%04x\n", registers.sp);
 	printf("PC: 0x%04x\n", registers.pc);
-	printf("IME: 0x%02x\n", registers.ime);
+	printf("IME: 0x%02x\n", interrupt.master);
+	printf("IE: 0x%02x\n", interrupt.enable);
+	printf("IF: 0x%02x\n", interrupt.flags);
 }
