d3309c9e42e6889f4229c973042361f8e3d9c231
CTurt <cturt@users.noreply.github.com>
2015-03-01 11:10:15 +0000

    Group registers in debugger

--- source/debug.c
+++ source/debug.c
@@ -23,17 +23,14 @@ void realtimeDebug(void) {
 	if(instructions[instruction].operandLength) debugMessageP += sprintf(debugMessageP, instructions[instruction].disassembly, operand);
 	else debugMessageP += sprintf(debugMessageP, instructions[instruction].disassembly);
 	
-	debugMessageP += sprintf(debugMessageP, "\n\nA: 0x%02x\n", registers.a);
-	debugMessageP += sprintf(debugMessageP, "F: 0x%02x\n", registers.f);
-	debugMessageP += sprintf(debugMessageP, "B: 0x%02x\n", registers.b);
-	debugMessageP += sprintf(debugMessageP, "C: 0x%02x\n", registers.c);
-	debugMessageP += sprintf(debugMessageP, "D: 0x%02x\n", registers.d);
-	debugMessageP += sprintf(debugMessageP, "E: 0x%02x\n", registers.e);
-	debugMessageP += sprintf(debugMessageP, "H: 0x%02x\n", registers.h);
-	debugMessageP += sprintf(debugMessageP, "L: 0x%02x\n", registers.l);
+	debugMessageP += sprintf(debugMessageP, "\n\nAF: 0x%02x\n", registers.af);
+	debugMessageP += sprintf(debugMessageP, "BC: 0x%02x\n", registers.bc);
+	debugMessageP += sprintf(debugMessageP, "DE: 0x%02x\n", registers.de);
+	debugMessageP += sprintf(debugMessageP, "HL: 0x%02x\n", registers.hl);
 	debugMessageP += sprintf(debugMessageP, "SP: 0x%04x\n", registers.sp);
 	debugMessageP += sprintf(debugMessageP, "PC: 0x%04x\n", registers.pc);
-	debugMessageP += sprintf(debugMessageP, "IME: 0x%02x\n", interrupt.master);
+	
+	debugMessageP += sprintf(debugMessageP, "\nIME: 0x%02x\n", interrupt.master);
 	debugMessageP += sprintf(debugMessageP, "IE: 0x%02x\n", interrupt.enable);
 	debugMessageP += sprintf(debugMessageP, "IF: 0x%02x\n", interrupt.flags);
 	
